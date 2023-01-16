baee304cf4be4820207b29bf13f7944e70be760f
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-17 02:09:22 +0300

    ActionTest: WIP

--- tests/ActionTest.php
+++ tests/ActionTest.php
@@ -3,21 +3,15 @@
 use Deligoez\EventMachine\Action;
 use Spatie\LaravelData\DataCollection;
 
-test('action', function () {
-    $action = Action::from(['name' => 'action_name']);
+test('create an action')
+    ->expect(fn() => Action::from(['name' => 'action_name']))
+    ->toBeInstanceOf(Action::class)
+    ->name->toBe('action_name');
 
-    expect($action)->toBeInstanceOf(Action::class);
-});
-
-test('actions', function () {
-    $actions = Action::collection([
+test('create multiple actions')
+    ->expect(fn() => Action::collection([
         ['name' => 'action_name_1'],
         ['name' => 'action_name_2'],
-    ]);
-
-    expect($actions)->toBeInstanceOf(DataCollection::class);
-
-    foreach ($actions as $action) {
-        expect($action)->toBeInstanceOf(Action::class);
-    }
-});
+    ]))
+    ->toBeInstanceOf(DataCollection::class)
+    ->each->toBeInstanceOf(Action::class);

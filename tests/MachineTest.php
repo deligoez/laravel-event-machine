68a64c194583dfb0577e6dd51c3bcbbba71e45c0
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-18 01:08:05 +0300

    WIP

--- tests/MachineTest.php
+++ tests/MachineTest.php
@@ -1,46 +1,11 @@
 <?php
 
-use Deligoez\EventMachine\Action;
 use Deligoez\EventMachine\Machine;
 use Deligoez\EventMachine\State;
 use Deligoez\EventMachine\Tests\Stubs\Machines\AuthenticationMachine;
-use Deligoez\EventMachine\Transition;
 
 test('create a machine')
-    ->expect(fn() => Machine::from([
-        'id'            => 'authentication_machine',
-        'initial_state' => 'logged_out_state',
-        'states'        => [
-            [
-                'name' => 'logged_out_state',
-                'transitions'   => [
-                    [
-                        'source_state' => 'logged_out_state',
-                        'target_state' => 'logged_in_state',
-                        'event'        => 'login_event',
-                        'actions'      => [
-                            ['name' => 'login_action_1'],
-                            ['name' => 'login_action_2'],
-                        ],
-                    ],
-                ],
-            ],
-            [
-                'name' => 'logged_in_state',
-                'transitions'   => [
-                    [
-                        'source_state' => 'logged_in_state',
-                        'target_state' => 'logged_out_state',
-                        'event'        => 'logout_event',
-                        'actions'      => [
-                            ['name' => 'logout_action_1'],
-                            ['name' => 'logout_action_2'],
-                        ],
-                    ],
-                ],
-            ],
-        ],
-    ]))
+    ->expect(fn() => Machine::from(AuthenticationMachine::getDefinition()))
     ->toBeInstanceOf(Machine::class)
     ->id->toBe('authentication_machine')
     ->states->each->toBeInstanceOf(State::class);

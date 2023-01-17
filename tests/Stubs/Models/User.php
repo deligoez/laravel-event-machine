58c11ab5ee50044e4cfe7ae61eed41d4b86573a1
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-17 08:32:21 +0300

    WIP

--- tests/Stubs/Models/User.php
+++ tests/Stubs/Models/User.php
@@ -3,7 +3,7 @@
 namespace Deligoez\EventMachine\Tests\Stubs\Models;
 
 use Deligoez\EventMachine\Tests\Stubs\Database\Factories\UserFactory;
-use Deligoez\EventMachine\Tests\Stubs\EventMachines\EmailVerificationEventMachine;
+use Deligoez\EventMachine\Tests\Stubs\EventMachines\EmailVerificationMachine2;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,7 +18,7 @@ class User extends Model
 
     protected $casts = [
         'email' => 'string',
-        'email_verified' => EmailVerificationEventMachine::class,
+        'email_verified' => EmailVerificationMachine2::class,
     ];
 
     protected static function newFactory(): UserFactory

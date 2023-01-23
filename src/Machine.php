c020d6e3c946de0ca5abbe945e3b19958196d0ea
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-24 02:45:57 +0300

    Start from scratch

--- src/Machine.php
+++ src/Machine.php
@@ -2,57 +2,14 @@
 
 namespace Deligoez\EventMachine;
 
-use Spatie\LaravelData\Attributes\DataCollectionOf;
-use Spatie\LaravelData\Data;
-use Spatie\LaravelData\DataCollection;
-
-class Machine extends Data
+class Machine
 {
     public function __construct(
-        public string $name,
-
-        public string $initial_state,
-
-        #[DataCollectionOf(State::class)]
-        public DataCollection $states,
     ) {
-        self::validate($this);
     }
 
-    public static function fromArray(array $data): self
+    public static function from(array $definition): self
     {
-        $states = [];
-        foreach ($data['states'] as $stateName => $state) {
-            $transitions = [];
-
-            foreach ($state['transitions'] as $event => $transition) {
-                if (is_array($transition)) {
-                    $transitions[] = [
-                        'source_state' => $stateName,
-                        'target_state' => $transition['target_state'],
-                        'event'        => $event,
-                        'actions'      => is_array($transition['actions'])
-                            ? $transition['actions']
-                            : [$transition['actions']],
-                    ];
-                } else {
-                    $transitions[] = [
-                        'source_state' => $stateName,
-                        'target_state' => $transition,
-                        'event'        => $event,
-                    ];
-                }
-            }
-            $states[] = [
-                'name'        => $stateName,
-                'transitions' => $transitions,
-            ];
-        }
-
-        return new self(
-            name: $data['name'],
-            initial_state: $data['initial_state'],
-            states: State::collection($states),
-        );
+        return new self();
     }
 }
--- tests/MachineTest.php
+++ tests/MachineTest.php
@@ -1,18 +1,11 @@
 <?php
 
 use Deligoez\EventMachine\Machine;
-use Deligoez\EventMachine\State;
-use Deligoez\EventMachine\Tests\Stubs\Machines\AuthenticationMachine;
-use Deligoez\EventMachine\Tests\Stubs\Machines\ToggleMachine;
 
-test('create a machine')
-    ->expect(fn() => Machine::from(AuthenticationMachine::getDefinition()))
-    ->toBeInstanceOf(Machine::class)
-    ->name->toBe('authentication_machine')
-    ->states->each->toBeInstanceOf(State::class);
+test('create a machine', function () {
+    $machineDefinition = [];
 
-test('create another machine')
-    ->expect(fn() => Machine::from(ToggleMachine::getDefinition()))
-    ->toBeInstanceOf(Machine::class)
-    ->name->toBe('toggle_machine')
-    ->states->each->toBeInstanceOf(State::class);
+    $machine = Machine::from($machineDefinition);
+
+    expect($machine)->toBeInstanceOf(Machine::class);
+});

147f9ef39fadef9fc2c640ce030a1a0b79adaa34
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-26 08:53:25 +0300

    a machine can have states with or without implementation

--- tests/StateTest.php
+++ tests/StateTest.php
@@ -57,3 +57,31 @@ test('a machine can have states without implementation', function () {
             ->states->toBeNull();
     }
 });
+
+test('a machine can have states with or without implementation', function () {
+    $machine = Machine::define([
+        'name'          => 'traffic_lights_machine',
+        'initial_state' => 'red',
+        'states'        => [
+            'red',
+            'yellow' => [],
+            'green',
+        ],
+    ]);
+
+    expect($machine)
+        ->toBeInstanceOf(State::class)
+        ->machine->toBe($machine)
+        ->parent->toBeNull()
+        ->states->toBeArray()
+                ->toHaveCount(3);
+
+    foreach ($machine->states as $stateName => $stateInstance) {
+        expect($stateInstance)
+            ->toBeInstanceOf(State::class)
+            ->name->toBe($stateName)
+            ->machine->toBe($machine)
+            ->parent->toBe($machine)
+            ->states->toBeNull();
+    }
+});

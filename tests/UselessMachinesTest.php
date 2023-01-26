cb57c0e022a5bdfbd81b09fa45e3bbc6f1da29c5
Yunus Emre Deligöz <ye@deligoz.me>
2023-01-27 00:42:01 +0300

    WIP

---
+++ tests/UselessMachinesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Deligoez\EventMachine\Machine;
+use Deligoez\EventMachine\State;
+
+test('a useless machine')
+    ->with('useless_machines')
+    ->expect(Machine::define())
+    ->toBeInstanceOf(State::class)
+    ->machine->toBeInstanceOf(State::class)
+    ->name->toBe(State::DEFAULT_NAME)
+    ->value->toBe(State::DEFAULT_NAME)
+    ->id->not()->toBeNull()
+    ->description->toBeNull()
+    ->parent->toBeNull()
+    ->initialState->toBeNull()
+    ->states->toBeNull();
+
+dataset('useless_machines', [
+    'no definition' => [
+        //
+    ],
+    'empty definition' => [
+        [],
+    ],
+    'empty name' => [
+        [
+            'name' => '',
+        ],
+    ],
+    'empty id' => [
+        [
+            'id' => '',
+        ],
+    ],
+    'empty description' => [
+        [
+            'description' => '',
+        ],
+    ],
+    'empty states I' => [
+        [
+            'states',
+        ],
+    ],
+    'empty states II' => [
+        [
+            'states' => [],
+        ],
+    ],
+]);
